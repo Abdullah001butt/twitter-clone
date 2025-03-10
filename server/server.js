2757a030bdac45a638f8a2a6a6e20d896ff08626
Abdullah <anasbutt2014@gmail.com>
2025-03-10 17:25:52 +0400

    Auth Done - 1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
@@ -7,6 +8,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
