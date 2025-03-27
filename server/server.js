88e22cdc66c0594994eb31dbbb55073d8a1e86db
Abdullah <anasbutt2014@gmail.com>
2025-03-27 13:05:47 +0400

    payload limit updated in server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
